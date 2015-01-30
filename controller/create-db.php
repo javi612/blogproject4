6fd67ddf89bab20e799420891cdb26db2e436edb
javier alvarez <javi_612@yahoo.com>
2015-01-30 14:02:54 -0800

    ugghh

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,11 +1,16 @@
 <?php
-    require_once("../model/database.php");
+    require_once(__DIR__ . "/../model/database.php");
 
-$connection = new myspli($host, $username, $password);
+$connection = new mysqli($host, $username, $password);
 
 if($connection->connect_error) {
     die("Error: " . $connection->connect.error);
 }
-else{
-    echo "Success";
+
+$exists = $connection->select_db($database);
+
+if(!$exists) {
+    echo "Database does not exists";
 }
+
+$connection->close();
