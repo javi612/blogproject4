75528dda3eb96e21b32192dced15368ba9e3905c
javier alvarez <javi_612@yahoo.com>
2015-02-05 18:15:35 -0800

    commit

--- controller/create-db.php
+++ controller/create-db.php
@@ -26,4 +26,10 @@ else {
     echo "Database already exists.";
 }
 
+$query = $connection->query("CREATE TABLE posts ("
+        . "id int(11) NOT NULL AUTO_INCREMENT"
+        . "title varchar (255) NOT NULL"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id)");
+
 $connection->close();
