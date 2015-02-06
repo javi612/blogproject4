fab26f19ba838c60450a61125753c94af4a70684
javier alvarez <javi_612@yahoo.com>
2015-02-06 14:01:07 -0800

    commit push

--- controller/create-db.php
+++ controller/create-db.php
@@ -9,7 +9,7 @@ $connection = new mysqli($host, $username, $password);
  */
 
 if($connection->connect_error) {
-    die("Error: " . $connection->connect.error);
+    die("<p>Error: " . $connection->connect_error . "</p>");
 }
 
 $exists = $connection->select_db($database);
@@ -18,18 +18,25 @@ if(!$exists) {
     $query = $connection->query("CREATE DATABASE $database");
     
     if($query){
-        echo "successfully created database: " . $database;
+        echo "<p>successfully created database: " . $database . "</p>";
     }
 }
 
 else {
-    echo "Database already exists.";
+    echo "<p>Database already exists.</p>";
 }
 
 $query = $connection->query("CREATE TABLE posts ("
-        . "id int(11) NOT NULL AUTO_INCREMENT"
-        . "title varchar (255) NOT NULL"
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "title varchar (255) NOT NULL,"
         . "post text NOT NULL,"
-        . "PRIMARY KEY (id)");
+        . "PRIMARY KEY (id))");
+
+if ($query) {
+    echo "<p>Successfully create table: posts</p>";
+}
+else {
+    echo "<p>$connection->error</p>";
+}
 
 $connection->close();
