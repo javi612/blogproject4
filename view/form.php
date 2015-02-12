dc4df25faa2921417fa2ced013888f77fcf3195c
javier alvarez <javi_612@yahoo.com>
2015-02-12 13:58:52 -0800

    hi

--- view/form.php
+++ view/form.php
@@ -1,8 +1,21 @@
 <?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<h1>Create Blog Post</h1>
 
+<form  method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
+    <div>
+        <label for="title">Title: </label>
+        <input type="text" name="title" />
+    </div>
+    
+    <div>
+        <label for="post">Post :</label>
+        <textarea name="post"></textarea>
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
