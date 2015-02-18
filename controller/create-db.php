8f0d55d3bc0209c099b8d7023e71ec3c5dae94e1
javier alvarez <javi_612@yahoo.com>
2015-02-17 22:22:41 -0800

    help

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,31 +1,6 @@
 <?php
     require_once(__DIR__ . "/../model/config.php");
 
-$connection = new mysqli($host, $username, $password);
-
-/*
- * connection error is a type of code that if
- *  theres an error the whole page dies
- */
-
-if($connection->connect_error) {
-    die("<p>Error: " . $connection->connect_error . "</p>");
-}
-
-$exists = $connection->select_db($database);
-
-if(!$exists) {
-    $query = $connection->query("CREATE DATABASE $database");
-    
-    if($query){
-        echo "<p>successfully created database: " . $database . "</p>";
-    }
-}
-
-else {
-    echo "<p>Database already exists.</p>";
-}
-
 $query = $connection->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar (255) NOT NULL,"
@@ -38,5 +13,3 @@ if ($query) {
 else {
     echo "<p>$connection->error</p>";
 }
-
-$connection->close();
--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,6 @@
 <?php
     require_once(__DIR__ . "/../model/config.php");
     
-    $connection = new mysqli($host, $username, $password, $database);
-
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
@@ -15,4 +13,3 @@
         echo "<p>$connection->error</p>";
     }
     
-    $connection->close();
